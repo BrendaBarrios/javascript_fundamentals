6a7190f3cb954db5cb39764cece3c1c403e73c05
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2020-12-31 01:08:39 -0600

    Use of variables

---
+++ clase1_variables.js
@@ -0,0 +1,25 @@
+console.log('Comenzando curso de fundamentos de javascript a 27-12-2020');
+
+// Declaracion y asignación en varias lineas
+
+var nombre = 'Brenda'
+var apellido = 'Barrios'
+
+var edad = 15 //numero
+
+edad = '15 años' //cambia a string
+
+var peso = 75
+
+
+// Declaración y asignación en una linea varias variables
+
+var seg_nombre = 'Yasmin', ap_materno = "Becerra"
+
+// javascript es debilmente tipado  puedes cambiar de string a entero y viceversa
+
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad + ' años')
+console.log('Hola ' + seg_nombre + ' ' + ap_materno)
+console.log('Tengo ' + edad )
